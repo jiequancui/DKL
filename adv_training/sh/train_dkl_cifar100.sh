d9bcb84dd909eed69aa1097beda84dc1f7465471
jiequan <jiequancui@gmail.com>
2024-06-10 14:08:49 +0800

    update

--- adv_training/sh/train_dkl_cifar100.sh
+++ adv_training/sh/train_dkl_cifar100.sh
@@ -12,15 +12,30 @@
 
 
 source activate py3.8_pt1.8.1
+
 python train_dkl_cifar100.py \
        --arch WideResNet34_10 \
        --data CIFAR100 \
        --train_budget 'high' \
-       --mark cifar100_dkl_a4b20t2 \
+       --mark cifar100_dkl_a4b20t4 \
        --epsilon 8 \
        --lr 0.2 \
        --beta 20.0 \
        --alpha 4.0 \
+       --T 4.0 
+
+python swa.py workdir/cifar100_dkl_a4b20t4 0.9 1
+
+python train_dkl_cifar100.py \
+       --arch WideResNet34_10 \
+       --data CIFAR100 \
+       --train_budget 'high' \
+       --mark cifar100_dkl_a5b20t2 \
+       --epsilon 8 \
+       --lr 0.2 \
+       --beta 20.0 \
+       --alpha 5.0 \
        --T 2.0 
 
-python swa.py workdir/cifar100_dkl_a4b20t2 0.9 1
+python swa.py workdir/cifar100_dkl_a5b20t2 0.9 1
+
