d4876022419312921ac6fb89a93809173a48e750
jiequan <jiequancui@gmail.com>
2024-10-06 02:43:34 +0800

    update

--- KD-dkl/sh/imagenet_r34_r18_ikl.sh
+++ KD-dkl/sh/imagenet_r34_r18_ikl.sh
@@ -5,9 +5,6 @@
 #SBATCH --mail-type=ALL
 #SBATCH --cpus-per-task=40
 #SBATCH --gres=gpu:8
-#SBATCH -p 2080
-#SBATCH -q 2080
-#SBATCH -x proj73
 
 source activate py3.8_pt1.8.1
 python3 tools/train.py --cfg imagenet_kd_dkl_configs/r34_r18_ikl_kd.yaml 
--- KD-dkl/sh/imagenet_r50_mv_ikl.sh
+++ KD-dkl/sh/imagenet_r50_mv_ikl.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=ikl_kd_r50_mv_
+#SBATCH --job-name=ikl_kd_r50_mv
 #SBATCH --mail-user=jiequancui@link.cuhk.edu.hk
 #SBATCH --output=imgnet.log
 #SBATCH --mail-type=ALL
 #SBATCH --cpus-per-task=40
 #SBATCH --gres=gpu:8
-#SBATCH -p 2080
-#SBATCH -q 2080
-#SBATCH -x proj73
 
 source activate py3.8_pt1.8.1
 python3 tools/train.py --cfg imagenet_kd_dkl_configs/r50_mv_ikl_kd.yaml 
